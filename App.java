a2d05d974a821f7ff3c5622aa241c76187267266
pranav <prhe22ise@cmrit.ac.in>
2025-06-01 16:56:23 +0530

    Initial commit - add Maven project files

---
+++ App.java
@@ -0,0 +1,10 @@
+package com.program2.maven.program2_example_jar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
